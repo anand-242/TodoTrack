db9aadad751674d72743db666a0bb02a30450a7c
Apeksha <apeksha4888@gmail.com>
2024-03-04 21:08:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { Header } from "./components/Header";
+import { AddTask } from "./components/AddTask";
+import { ShowTask } from "./components/ShowTask";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [task, setTask] = useState(null);
+  const [tasklist, setTasklist] = useState(() => {
+    try {
+      const storedTasklist = localStorage.getItem("tasklist");
+      return storedTasklist ? JSON.parse(storedTasklist) : [];
+    } catch (error) {
+      console.error("Error parsing tasklist from localStorage:", error);
+      return [];
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem("tasklist", JSON.stringify(tasklist));
+  }, [tasklist]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <AddTask
+        tasklist={tasklist}
+        setTasklist={setTasklist}
+        task={task}
+        setTask={setTask}
+      />
+      <ShowTask
+        tasklist={tasklist}
+        setTasklist={setTasklist}
+        task={task}
+        setTask={setTask}
+      />
     </div>
   );
 }
